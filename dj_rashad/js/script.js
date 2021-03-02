31b33d71f9f1174fee3aad392b072278e4bbf38f
Erik Borg <e.borg1990@gmail.com>
2021-03-02 15:36:11 +0300

    added practice

---
+++ dj_rashad/js/script.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor = "#696969";
+const page = document.querySelector('.page');
+page.style.backgroundColor = "ghostwhite";
+document.querySelector('.name').textContent = "DJ Rashad";
+const image = document.querySelector('img');
+image.setAttribute('src', 'https://media.npr.org/assets/img/2014/04/28/rashad-photo-credit-ashes_57_wide-1682baac9841866ec5422f6a5f54082336d0a0d1-s800-c85.jpg');
+const bio = document.querySelector('.short-bio');
+bio.className += ' animated';
+
