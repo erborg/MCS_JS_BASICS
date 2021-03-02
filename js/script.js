a60fd333b862f307cb446fff75b14d55268d6d9a
Erik Borg <e.borg1990@gmail.com>
2021-03-02 14:42:33 +0300

    small fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let totalCash = parseInt(prompt('Total Cash'));
-let watchesCount = parseInt(prompt('Watches Count'));
-let earringsCount = parseInt(prompt('Earrings Count'));
-let watchesSum = parseInt(prompt('Watches Cost')) * watchesCount;
-let earringsSum = parseInt(prompt('Earrings Cost')) * earringsCount;
-let totalSum = alert (watchesSum + earringsSum);
+const totalCash = parseInt(prompt('Total Cash'));
+const watchesCount = parseInt(prompt('Watches Count'));
+const earringsCount = parseInt(prompt('Earrings Count'));
+const watchesSum = parseInt(prompt('Watches Cost')) * watchesCount;
+const earringsSum = parseInt(prompt('Earrings Cost')) * earringsCount;
+const totalSum = alert(watchesSum + earringsSum);
 document.write(totalCash >= totalSum);
