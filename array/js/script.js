e5c607c0fe463e06e8d2bf8e3784da2b40117752
Erik Borg <e.borg1990@gmail.com>
2021-03-11 18:01:24 +0300

    use forEach

--- array/js/script.js
+++ array/js/script.js
@@ -14,10 +14,10 @@ let crypto = [
 ]
 
 const color = ["#D0021D", "#4991E2", "#50E3C1"]
-for(let i = 0; i < crypto.length; i++) {
+crypto.forEach(function(coin, i, crypto) {
   document.querySelector('body').innerHTML += "<div class='coin'><h1>"+crypto[i].name+"</h1><div>"+crypto[i].price+"</div><div class='stripe'></div></div>"
   for (stripe of document.querySelectorAll('.stripe')) {
     stripe.style.width += crypto[i].price/2+'px'
     stripe.style.backgroundColor += color[i]
   }
-}
+})
