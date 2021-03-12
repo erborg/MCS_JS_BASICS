4e05acfab5f4f43653963b8826765469309d322f
Erik Borg <e.borg1990@gmail.com>
2021-03-12 11:59:42 +0300

    move to %

--- array/js/script.js
+++ array/js/script.js
@@ -17,7 +17,7 @@ const color = ["#D0021D", "#4991E2", "#50E3C1"]
 crypto.forEach(function(coin, i, crypto) {
   document.querySelector('body').innerHTML += "<div class='coin'><h1>"+crypto[i].name+"</h1><div>"+crypto[i].price+"</div><div class='stripe'></div></div>"
   for (stripe of document.querySelectorAll('.stripe')) {
-    stripe.style.width += crypto[i].price/2+'px'
+    stripe.style.width += crypto[i].price * 0.05 + '%'
     stripe.style.backgroundColor += color[i]
   }
 })
